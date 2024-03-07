6367d049f13ae5d1284f25646f3df6736f626de7
hussamkhaled <hussam.h.khaled@gmail.com>
2024-03-07 10:17:37 +0200

    clean code

--- Store/ActionCreator/Constants.js
+++ Store/ActionCreator/Constants.js
@@ -2,10 +2,10 @@
 //  export const server = 'http://192.168.43.95:8081';
 
 // export const server = 'http://45.9.190.133:8081';
-//export const server = 'https://services.facilify.com.au';
+export const server = 'https://services.facilify.com.au';
 
 
-export const server = 'http://45.9.190.133:8081';
+// export const server = 'http://45.9.190.133:8081';
 
 
 export const privatePath ='/avh/nufm/v1/public';
